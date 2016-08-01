9e8ff9f01f3607a9d37698b2b0c960ab9dcb90fb
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-08-01 11:49:17 +0700

    search by keyword function - keep keyword after search

--- http/controller/book-controller.js
+++ http/controller/book-controller.js
@@ -37,7 +37,7 @@ exports.update = function (req, res) {
 
 exports.search = function (req, res) {
     req.app.bookRepository.search(req.app.condition).then(function(books){
-        res.render('home.nj.html', {books: books});
+        res.render('home.nj.html', {books: books, condition: req.app.condition});
     });
     // console.log(req.app.condition);
 };
